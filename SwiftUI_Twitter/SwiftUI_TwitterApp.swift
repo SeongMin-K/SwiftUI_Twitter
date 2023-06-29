c557572f529089758c0c81deb4382cb86df037e6
SeongMin-K <eo1tks3@naver.com>
2023-06-29 14:46:56 +0900

    Initial Commit

---
+++ SwiftUI_Twitter/SwiftUI_TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TwitterApp.swift
+//  SwiftUI_Twitter
+//
+//  Created by SeongMinK on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
